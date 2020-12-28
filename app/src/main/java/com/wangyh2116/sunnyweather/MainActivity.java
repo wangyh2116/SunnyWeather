c9d0db9b80c460eae63abdf4e5ed21a4b66e1435
Wangyh2116 <17843107345@163.com>
2020-12-28 10:37:20 +0800

    First commit

---
+++ app/src/main/java/com/wangyh2116/sunnyweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wangyh2116.sunnyweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
